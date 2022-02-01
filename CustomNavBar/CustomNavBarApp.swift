d8efd2b784ef8c2a4c4d6cdaea6bd03c65210eda
FlorianTo <florian.tousch@hotmail.fr>
2022-02-01 17:51:32 +0100

    Initial Commit

---
+++ CustomNavBar/CustomNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarApp.swift
+//  CustomNavBar
+//
+//  Created by Florian on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
