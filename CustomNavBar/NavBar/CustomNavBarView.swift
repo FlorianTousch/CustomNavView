23236ddfa9c45cdb910c1480cbd9c4615079b42d
FlorianTo <florian.tousch@hotmail.fr>
2022-02-01 18:07:32 +0100

    Extract code to variable

--- CustomNavBar/NavBar/CustomNavBarView.swift
+++ CustomNavBar/NavBar/CustomNavBarView.swift
@@ -10,29 +10,12 @@ import SwiftUI
 struct CustomNavBarView: View {
     var body: some View {
         HStack {
-            Button {
-                
-            } label: {
-                Image(systemName: "chevron.left")
-            }
-            
+            backButton
             Spacer()
-            
-            VStack(spacing: 4) {
-                Text("Title")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Description")
-            }
-            
+            titleSection
             Spacer()
-            
-            Button {
-                
-            } label: {
-                Image(systemName: "chevron.left")
-            }
-            .opacity(0)
+            backButton
+                .opacity(0)
         }
         .padding()
         .accentColor(.white)
@@ -49,6 +32,26 @@ struct CustomNavBarView_Previews: PreviewProvider {
         VStack {
             CustomNavBarView()
             Spacer()
+        }
+    }
+}
+
+extension CustomNavBarView {
+    
+    private var backButton: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "chevron.left")
+        }
+    }
+    
+    private var titleSection: some View {
+        VStack(spacing: 4) {
+            Text("Title")
+                .font(.title)
+                .fontWeight(.semibold)
+            Text("Description")
         }
     }
 }
